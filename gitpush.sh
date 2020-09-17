2502af95e23e0811078f762c84b9dc0d8dca780a
lqwangxg <lqwangxg@gmail.com>
2020-09-17 04:57:04 +0000

    commit

--- gitpush.sh
+++ gitpush.sh
@@ -1,6 +1,22 @@
 git config --global user.email "lqwangxg@gmail.com"
 git config --global user.name "lqwangxg"
 
+#==========================
+branch=master
+comment=commit
+if [ $1 eq "-b" ]; then 
+  branch=$2
+elif [ $1 eq "-m" ]; then 
+  comment=$2
+fi
+
+if [ $3 eq "-b" ]; then 
+  branch=$4
+elif [ $3 eq "-m" ]; then 
+  comment=$4
+fi
+#=------------------------
+
 git add . 
-git commit -a -m "commit"
-git push -u origin master 
+git commit -a -m ${comment}
+git push -u origin ${branch}
